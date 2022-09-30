7f43a6f66eeacb5fdefaaa38375c5c951de2b29b
Pratham <109359719+pratham15541@users.noreply.github.com>
2022-09-30 13:40:43 +0530

    final commit

--- js/script.js
+++ js/script.js
@@ -4,7 +4,7 @@ let audioturn = new Audio("static/mp3/ting (mp3cut.net).mp3");
 let gameover = new Audio("static/mp3/won.wav");
 let turn = "X";
 let isgameover = false;
-
+let resetgame = true
 // fuction to cahnge turn
 const changeturn = () => {
   return turn === "X" ? "O" : "X";
@@ -14,16 +14,19 @@ const changeturn = () => {
 const checkwin = () => {
   let boxtxts = document.getElementsByClassName("boxtxt");
   let wins = [
-    [0, 1, 2],
-    [3, 4, 5],
-    [6, 7, 8],
-    [0, 3, 6],
-    [1, 4, 7],
-    [2, 5, 8],
-    [0, 4, 8],
-    [2, 4, 6],
+    [0, 1, 2, 3, 5, 0],
+    [3, 4, 5, 3, 15, 0],
+    [6, 7, 8, 3, 25, 0],
+    [0, 3, 6, -6.3, 15, 90],
+    [1, 4, 7, 3.7, 15, 90],
+    [2, 5, 8, 13.7, 15, 90],
+    [0, 4, 8, 4, 15, 45],
+    [2, 4, 6, 4, 14, 135],
   ];
   wins.forEach((e) => {
+    {
+      res
+      if(resetgame==false)
     if (
       boxtxts[e[0]].innerText === boxtxts[e[1]].innerText &&
       boxtxts[e[2]].innerText === boxtxts[e[1]].innerText &&
@@ -31,11 +34,15 @@ const checkwin = () => {
     ) {
       document.querySelector(".info").innerText =
         boxtxts[e[0]].innerText + " Won ";
-      isgameover = true;
+
       document
         .querySelector(".imgbox")
         .getElementsByTagName("img")[0].style.width = " 150px";
-        
+      document.querySelector(
+        ".line"
+      ).style.transform = `translate(${e[3]}vw, ${e[4]}vw) rotate(${e[5]}deg)`;
+      document.querySelector(".line").style.width = `23vw`;
+    }
     }
   });
 };
@@ -60,21 +67,18 @@ Array.from(boxes).forEach((element) => {
 
 //reset button
 
-
-  // window.location.reload();
-reset = document.getElementsByClassName('reset')[0]
-reset.addEventListener("click", () => {
-  
-  let boxtxt = document.querySelectorAll(".boxtxt");
-  Array.from(boxtxt).forEach((element) => {
-    element.innerText = "";
-   
-  
-
+// window.location.reload();
+if(resetgame==true){
+  reset = document.getElementsByClassName("reset")[0];
+  reset.addEventListener("click", () => {
+    let boxtxt = document.querySelectorAll(".boxtxt");
+    Array.from(boxtxt).forEach((element) => {
+      element.innerText = "";
+    });
   });
-});
-
-
-function he(){
-  window.location.reload()
+  
+  function he() {
+    window.location.reload();
+  }
 }
+
